ad40fc23720a14f5761ce865db4470e2aad3325f
YDima <yurchenkodmytroofficial@gmail.com>
2021-01-20 13:48:50 +0200

    Crypto

--- crawler.py
+++ crawler.py
@@ -2,8 +2,9 @@ import urllib.request
 from bs4 import BeautifulSoup
 import threading
 
+
 def find_links():
-    hobbies = ["rashomon", "hokey", "kurosawa", "david+lynch", "twin+peaks"]
+    hobbies = ["rashomon", "hockey", "kurosawa", "david+lynch", "twin+peaks"]
 
     for i in hobbies:
         query = i + " article"
@@ -15,10 +16,12 @@ def find_links():
         request = urllib.request.Request(url)
 
         # Set a normal User Agent header, otherwise Google will block the request.
-        request.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36')
+        request.add_header('User-Agent',
+                           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) '
+                           'Chrome/87.0.4280.88 Safari/537.36')
         raw_response = urllib.request.urlopen(request).read()
 
-        # Read the repsonse as a utf-8 string
+        # Read the response as a utf-8 string
         html = raw_response.decode("utf-8")
         soup = BeautifulSoup(html, 'html.parser')
 
@@ -28,10 +31,11 @@ def find_links():
             # Search for a h3 tag
             results = div.select("h3")
 
-            if (len(results) >= 1):
+            if len(results) >= 1:
                 h3 = results[0]
                 print(h3.get_text())
                 print(div.get('href'))
+
 
 if __name__ == "__main__":
     t = threading.Thread(target=find_links)
